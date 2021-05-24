4b8842eed5993f05f386c570c1464c84ce6b6a58
Kris Tung <ragdoll.washoku89@gmail.com>
2021-05-24 22:51:38 +0800

    更正子元件傳入父元件的觸發事件(click改為close才對)

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <h1>{{ title }}</h1>
   <div v-if="showModal">
     <!--子元件Modal有兩個屬性: header & text-->
-    <Modal :header="header" :text="text" theme="sale" @click="toggleModal"/>
+    <Modal :header="header" :text="text" theme="sale" @close="toggleModal"/>
     <!--@click=""是從子元件emit來的，其目的為「點擊灰色區域後要關掉Modal，而因為其指令目的與父元件中的button效用一樣，故寫成@click="toggleModal"，等於子元件的closeModal函式要在點擊後去呼叫父元件中的toggleModal這個程式-->
   </div>
   <br>
